8584e2ee480062bbb890d053c88d1223659a5bd8
tahzeer <tahzeer.ash@gmail.com>
2025-11-19 18:50:37 +0530

    refactor: update partner services and helpers

--- openg2p-registry-core/src/openg2p_registry_core/helpers/signature_pattern_matcher.py
+++ openg2p-registry-core/src/openg2p_registry_core/helpers/signature_pattern_matcher.py
@@ -1,6 +1,5 @@
 import re
-from sqlalchemy import select
-from typing import Tuple, Dict, List
+from typing import Tuple, Dict
 from jsonpath_ng import parse as jsonpath_parse
 
 from openg2p_fastapi_common.service import BaseService
@@ -18,69 +17,31 @@ class SignaturePatternMatcher(BaseService):
         pattern_for_data_model = "$.body.meta.data_model_is::^[A-Z]+$"
     """
 
-    async def match(self, ingest_data: Dict, session) -> Tuple[
-        IncomingModelSignaturePattern, str, str, str
-    ]:
-        patterns: List[IncomingModelSignaturePattern] = await self._get_all_model_signature_patterns(session)
+    def match(
+        self, incoming_model_signature_pattern: IncomingModelSignaturePattern, ingest_data: Dict
+    ) -> Tuple[str, str]:
 
-        for pattern in patterns:
-            # If ANY one of the three doesn't match -> try next pattern
-            sender = self._extract_with_pattern(
-                ingest_data, pattern.pattern_for_sender
-            )
-            if sender is None:
-                continue
+        sender = self._extract_with_pattern(
+            ingest_data, incoming_model_signature_pattern.pattern_for_sender
+        )
+        signature = self._extract_with_pattern(
+            ingest_data, incoming_model_signature_pattern.pattern_for_signature
+        )
+        return sender, signature
 
-            signature = self._extract_with_pattern(
-                ingest_data, pattern.pattern_for_signature
-            )
-            if signature is None:
-                continue
-
-            data_model = self._extract_with_pattern(
-                ingest_data, pattern.pattern_for_data_model
-            )
-            if data_model is None:
-                continue
-
-            # TODO: Check weather sender matches the partner_mnemonic stored in db for this pattern
-            # partner_mnemonic = await self._get_partner_mnemonic(pattern.partner_id, session)
-            # if partner_mnemonic != sender:
-            #     continue
-            
-            # If ALL three matched -> pattern found
-            return (pattern, sender, signature, data_model)
-
-        # No match found
-        return (None, None, None, None)
-
-
-    async def _get_all_model_signature_patterns(
-        self, session
-    ) -> List[IncomingModelSignaturePattern]:
-
-        patterns = await session.execute(select(IncomingModelSignaturePattern))
-        return patterns.scalars().all()
 
     def _extract_with_pattern(self, data: Dict, pattern_str: str):
         try:
             jsonpath_expr, regex_expr = pattern_str.split("::", 1)
+            value = self._extract_jsonpath(data, jsonpath_expr)
+            if value is None:
+                return None
+            return self._validate_regex(value, regex_expr)
+
         except ValueError:
             raise ValueError("Invalid pattern format")
-
-        value = self._extract_jsonpath(data, jsonpath_expr)
-        if value is None:
-            return None
-
-        try:
-            pattern = re.compile(regex_expr)
         except Exception as e:
             raise e
-
-        if not pattern.fullmatch(str(value)):
-            return None
-
-        return value
 
     def _extract_jsonpath(self, data: Dict, jsonpath_expr: str):
         try:
@@ -89,7 +50,12 @@ class SignaturePatternMatcher(BaseService):
             return matches[0].value if matches else None
         except Exception as e:
             raise e
-
-    # TODO: Add this logic after partner models are established
-    # async def _get_partner_mnemonic(self, partner_id: str, session: Session) -> str:
-    #     pass
+    
+    def _validate_regex(self, value: str, regex_expr: str):
+        try:
+            pattern = re.compile(regex_expr)
+            if not pattern.fullmatch(str(value)):
+                return None
+            return value
+        except Exception as e:
+            raise e
