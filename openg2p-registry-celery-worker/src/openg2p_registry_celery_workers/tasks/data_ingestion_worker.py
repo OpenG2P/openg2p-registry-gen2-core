f184429b6f8fe8d0e881476477573fc32a828f0a
tahzeer <tahzeer.ash@gmail.com>
2025-11-25 12:23:25 +0530

    feat: add logic for data_ingestion_worker

--- openg2p-registry-celery-worker/src/openg2p_registry_celery_workers/tasks/data_ingestion_worker.py
+++ openg2p-registry-celery-worker/src/openg2p_registry_celery_workers/tasks/data_ingestion_worker.py
@@ -1,12 +1,14 @@
-import json
 import logging
-from typing import List
+import asyncio
+from asyncio import AbstractEventLoop
 
+from openg2p_registry_core.schemas import ChangeLogPayload
 from openg2p_registry_core.services import G2PRegisterService
-from sqlalchemy import func, select
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+from sqlalchemy.orm import Session, sessionmaker
 from openg2p_registry_core.models import (
     ProcessStatusEnum, 
+    G2PRegisterDefinition,
     IncomingClassifiedData, 
     IncomingEnrichedTransformedData
 )
@@ -33,11 +35,21 @@ def data_ingestion_worker(ingest_id: str):
             incoming_classified_data = session.get(IncomingClassifiedData, ingest_id)
             incoming_enriched_transformed_data = session.get(IncomingEnrichedTransformedData, ingest_id)
             
-            # Logic to ingest data using register services
-            # g2p_register_service = G2PRegisterService().get_component()
-            
+            change_log_payload = _construct_change_log_payload(
+                incoming_classified_data,
+                incoming_enriched_transformed_data,
+                session
+            )
+
+            g2p_register_service = G2PRegisterService().get_component()
+            async_loop_create_change_log(
+                g2p_register_service,
+                change_log_payload,
+                incoming_classified_data.partner_id
+            )
 
             # Update incoming_classified_data ingestion_status -> PROCESSED
+            incoming_classified_data.ingestion_number_of_attempts += 1
             incoming_classified_data.ingestion_status = ProcessStatusEnum.PROCESSED.value
             incoming_classified_data.ingestion_date_time = func.now()
             session.commit()
@@ -49,8 +61,14 @@ def data_ingestion_worker(ingest_id: str):
             # Rollback all sessions
             session.rollback()
 
-            # Update incoming_classified_data ingestion_status -> FAILED
-            incoming_classified_data.ingestion_status = ProcessStatusEnum.FAILED.value
+            # Retry logic if maximum attempts not exhausted
+            if incoming_classified_data.ingestion_number_of_attempts < _config.worker_max_attempts:
+                incoming_classified_data.ingestion_number_of_attempts += 1
+                incoming_classified_data.ingestion_status = ProcessStatusEnum.PENDING.value
+            else:
+                incoming_classified_data.ingestion_status = ProcessStatusEnum.FAILED.value
+
+            incoming_classified_data.ingestion_latest_error_code = str(e)
             incoming_classified_data.ingestion_date_time = func.now()
             session.commit()
             # Raise exception for testing
@@ -59,3 +77,32 @@ def data_ingestion_worker(ingest_id: str):
         _logger.info(
             f"Completed processing data_ingestion_worker for ingest_id: {ingest_id}"
         )
+
+
+def _construct_change_log_payload(
+    incoming_classified_data: IncomingClassifiedData,
+    incoming_enriched_transformed_data: IncomingEnrichedTransformedData,
+    session: Session
+) -> ChangeLogPayload:
+    g2p_register_definition = session.get(G2PRegisterDefinition, incoming_classified_data.register_id)
+    return ChangeLogPayload(
+        register_id=incoming_classified_data.register_id,
+        register_mnemonic=g2p_register_definition.register_mnemonic,
+        operation_id=incoming_classified_data.operation_id,
+        change_payload=incoming_enriched_transformed_data.transformed_data_json
+    )
+
+def async_loop_create_change_log(
+    g2p_register_service: G2PRegisterService,
+    change_log_payload: ChangeLogPayload,
+    source_partner_id: str
+):
+    loop: AbstractEventLoop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(
+        g2p_register_service.create_change_log(
+            change_log_payload=change_log_payload,
+            source_partner_id=source_partner_id
+        )
+    )
+    loop.close()
