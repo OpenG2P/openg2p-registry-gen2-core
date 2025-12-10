acfaf1eb13e3b797477ca310532cf051f7883188
tahzeer <tahzeer.ash@gmail.com>
2025-12-10 14:43:54 +0530

    refactor: rename core controller and services for partner api

--- openg2p-registry-core/src/openg2p_registry_core/controller_services/g2p_ingest_controller_service.py
+++ openg2p-registry-core/src/openg2p_registry_core/controller_services/g2p_ingest_controller_service.py
@@ -5,17 +5,17 @@ from openg2p_fastapi_common.service import BaseService
 from openg2p_fastapi_common.context import dbengine
 
 from ..models import IncomingRawData
-from ..services import G2PPartnerService
+from ..services import G2PIngestService
 from ..schemas import IngestDataPayload
 
 _logger = logging.getLogger('g2p-partner-contoller-service')
 _engine = dbengine.get()
 
-class G2PPartnerControllerService(BaseService):
+class G2PIngestControllerService(BaseService):
 
     async def ingest_data(self, data_model: Optional[str], ingest_data: Dict) -> IngestDataPayload:
-        g2p_partner_service = G2PPartnerService.get_component()
-        incoming_raw_data: IncomingRawData = await g2p_partner_service.ingest_data(data_model.upper() if data_model else None, ingest_data)
+        g2p_ingest_service = G2PIngestService.get_component()
+        incoming_raw_data: IncomingRawData = await g2p_ingest_service.ingest_data(data_model.upper() if data_model else None, ingest_data)
 
         ingest_data_payload = IngestDataPayload(ingest_id=incoming_raw_data.ingest_id)
         return ingest_data_payload    
