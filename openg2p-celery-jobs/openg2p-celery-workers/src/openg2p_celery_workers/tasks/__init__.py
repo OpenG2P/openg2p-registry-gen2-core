5f39daec26908403f2f76578fba0c297e88f2ca5
tahzeer <tahzeer.ash@gmail.com>
2025-12-17 11:10:16 +0530

    refactor: rename celery worker for registry worker

--- openg2p-celery-jobs/openg2p-celery-workers/src/openg2p_celery_workers/tasks/__init__.py
+++ openg2p-celery-jobs/openg2p-celery-workers/src/openg2p_celery_workers/tasks/__init__.py
@@ -1,2 +1,2 @@
-from .g2p_register_external_data_q_worker import g2p_register_external_data_q_worker
+from .g2p_register_ingest_worker import g2p_register_ingest_worker
 from .g2p_external_data_poller_worker import g2p_external_data_poller_worker
