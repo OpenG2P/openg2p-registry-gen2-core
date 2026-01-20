f2bbd7f5d38b4b2b2f725cba47b2e574a598fb38
tahzeer <tahzeer.ash@gmail.com>
2026-01-20 16:59:07 +0530

    refactor: modify register queries to exclude table registers and order by register rank

--- openg2p-registry-core/src/openg2p_registry_core/services/g2p_register_service.py
+++ openg2p-registry-core/src/openg2p_registry_core/services/g2p_register_service.py
@@ -889,7 +889,10 @@ class G2PRegisterService(BaseService):
 
     async def _fetch_register_summary_data(self, session) -> list[RegisterSummaryData]:
         register_definitions: list[G2PRegisterDefinition] = (
-            await session.execute(select(G2PRegisterDefinition))
+            await session.execute(
+                select(G2PRegisterDefinition)
+                .where(G2PRegisterDefinition.register_purpose != RegisterPurposeEnum.TABLE.value)
+            )
         ).scalars().all()
 
         register_summary_data_list: list[RegisterSummaryData] = []
@@ -959,7 +962,11 @@ class G2PRegisterService(BaseService):
 
     async def _fetch_all_registers(self, session) -> list[RegisterData]:
         register_definitions: list[G2PRegisterDefinition] = (
-            await session.execute(select(G2PRegisterDefinition).where(G2PRegisterDefinition.register_purpose != RegisterPurposeEnum.TABLE.value))
+            await session.execute(
+                select(G2PRegisterDefinition)
+                .where(G2PRegisterDefinition.register_purpose != RegisterPurposeEnum.TABLE.value)
+                .order_by(G2PRegisterDefinition.register_rank)
+            )
         ).scalars().all()
 
         all_registers_list: list[RegisterData] = []
