7d4f44d65958b9cf80a2e4592c850afe7438d890
PSNAppZ <thepsnarayanan@gmail.com>
2026-01-21 14:35:05 +0530

    fix: update functional_record_id to be optional in RecordData model

--- openg2p-registry-core/src/openg2p_registry_core/schemas/payload.py
+++ openg2p-registry-core/src/openg2p_registry_core/schemas/payload.py
@@ -191,7 +191,7 @@ class RecordData(BaseModel):
     model_config = ConfigDict(extra="allow", from_attributes=True)
 
     internal_record_id: str
-    functional_record_id: str
+    functional_record_id: Optional[str] = None
     link_internal_record_id: Optional[str] = None
     foundational_id: Optional[str] = None
     link_foundational_id: Optional[str] = None
