1cea865a4049fd6c6597bcd5acb8431d46e32e21
vin0dkhichar <abbb61667@gmail.com>
2026-04-24 15:39:06 +0530

    G2P-4668 Add G2PGeoHierarchyService in app.py

--- openg2p-registry-core/src/openg2p_registry_core/app.py
+++ openg2p-registry-core/src/openg2p_registry_core/app.py
@@ -88,6 +88,7 @@ from .services import (
     G2PUIHelperService,
     G2PVcConfigurationService,
     G2PChangeRequestCoreService,
+    G2PGeoHierarchyService
 )
 
 _config = Settings.get_config(strict=False)
@@ -133,6 +134,7 @@ class Initializer(BaseInitializer):
         G2PRegisterVerificationService()
         G2PChangeRequestCoreService()
         G2PChangeRequestWorkerService()
+        G2PGeoHierarchyService()
 
         # Controller Services
         G2PDataModelControllerService()
